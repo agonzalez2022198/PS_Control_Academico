902fc0587c4964df784b4f14bc9390c56c17785c
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2024-02-14 13:19:55 -0600

    Estudiante cambios

---
+++ models/estudiante.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const EstudianteSchema = Schema({
+
+    nombre: {
+        type: String,
+        required: [true, 'Ingresa tu nombre']
+    },
+
+    apellidos: {
+        type: String,
+        required: [true, 'Ingresa tus apellidos']
+    },
+
+    carnet: {
+        type: String,
+        required: [true, 'No puedes dejar vacío.']
+    },
+
+    correo: {
+        type: String,
+        required: [true, 'Necesitar llenar es te espacio.'],
+        unique: true
+    },
+
+    telefono: {
+        type: String,
+        required: [true, 'El telefono es obligatorio.']
+    },
+
+    estado:{
+        type: Boolean,
+        default: true
+    }
+
+
+});
+
+
+EstudianteSchema.methods.toJSON = function(){
+    const {__v, carnet, _id, ...correo } = this.toObject();
+    usuario.uid = _id;
+    return usuario;
+
+};
+
+module.exports = model ('Estudiante', EstudianteSchema);
